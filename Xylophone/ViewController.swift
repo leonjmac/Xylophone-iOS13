4a11a6e39a42087ecc8c2b9dfbe05ca128bbefb4
Leon J Mac <leonjmac@protonmail.ch>
2020-04-20 23:31:58 +0100

    Update with function to handle button press with animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.isEnabled.toggle()
+        fadeButton(sender)
+        playSound(inKey: (sender.titleLabel?.text)!)
+    }
+    
+    func playSound(inKey key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    func fadeButton(_ btn: UIButton) {
+        UIView.animate(withDuration: 1.2, delay: 0.2, options: [.curveEaseInOut, .autoreverse], animations: {
+            btn.alpha = 0.5
+        }, completion: { finished in
+            btn.isEnabled.toggle()
+            btn.alpha = 1.0
+        })
+    }
 }
-
